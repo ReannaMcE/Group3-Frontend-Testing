e5bb7fd8de3f4f83d0a773c9971a1feac0adac21
Reanna <reanna.mceldowney@kainos.com>
2024-07-29 16:01:09 +0100

    tests passinggit add .

--- test/ui/ViewJobRolesTestPage.ts
+++ test/ui/ViewJobRolesTestPage.ts
@@ -1,58 +1,58 @@
-//this 'page' file is made to take all the values from webpage to use for the automated tests
+// //this 'page' file is made to take all the values from webpage to use for the automated tests
 
-import { By, WebDriver } from 'selenium-webdriver';
+// import { By, WebDriver } from 'selenium-webdriver';
 
-export class ViewJobRolesPage {
-    private driver: WebDriver;
+// export class ViewJobRolesPage {
+//     private driver: WebDriver;
 
-  // this just needs to load the view job roles page + assert that all the job roles are 'open' + check social links
-    // - there aren't any buttons to press as yet
+//   // this just needs to load the view job roles page + assert that all the job roles are 'open' + check social links
+//     // - there aren't any buttons to press as yet
 
-    // Locators
-    // ***** change these to match the view job roles page items NOT THE HOME PAGE
-    private facebookLink: By = By.xpath("//a[@title='https://www.facebook.com/KainosSoftware/?locale=en_GB']");
-    private twitterLink: By = By.xpath("//a[@title='https://x.com/i/flow/login?redirect_after_login=%2FKainosSoftware']");
-    private instagramLink: By = By.xpath("//a[@title='https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fkainossoftware%2F&is_from_rle']");
-    private title: By = By.xpath("//h2[normalize-space()='Available Job Roles']");
+//     // Locators
+//     // ***** change these to match the view job roles page items NOT THE HOME PAGE
+//     private facebookLink: By = By.xpath("//a[@title='https://www.facebook.com/KainosSoftware/?locale=en_GB']");
+//     private twitterLink: By = By.xpath("//a[@title='https://x.com/i/flow/login?redirect_after_login=%2FKainosSoftware']");
+//     private instagramLink: By = By.xpath("//a[@title='https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fkainossoftware%2F&is_from_rle']");
+//     private title: By = By.xpath("//h2[normalize-space()='Available Job Roles']");
 
-    // create the webdriver instance
-    constructor(driver: WebDriver) {
-        this.driver = driver;
-    }
+//     // create the webdriver instance
+//     constructor(driver: WebDriver) {
+//         this.driver = driver;
+//     }
 
-    // Actions
+//     // Actions
 
-    // ****** change these to match the view job roles page NOT THE HOME PAGE
+//     // ****** change these to match the view job roles page NOT THE HOME PAGE
 
-    async open(url: string): Promise<void> {
-        await this.driver.get(url);
-    }
+//     async open(url: string): Promise<void> {
+//         await this.driver.get(url);
+//     }
 
-    // click the facebook button to check
-    async clickFacebook(): Promise<void> {
-        const element = await this.driver.findElement(this.facebookLink);
-        await element.click();
-    }
+//     // click the facebook button to check
+//     async clickFacebook(): Promise<void> {
+//         const element = await this.driver.findElement(this.facebookLink);
+//         await element.click();
+//     }
 
-    // click the facebook button to check
-    async clickTwitter(): Promise<void> {
-        const element = await this.driver.findElement(this.twitterLink);
-        await element.click();
-    }
+//     // click the facebook button to check
+//     async clickTwitter(): Promise<void> {
+//         const element = await this.driver.findElement(this.twitterLink);
+//         await element.click();
+//     }
 
-    // click the facebook button to check
-    async clickInstagram(): Promise<void> {
-        const element = await this.driver.findElement(this.instagramLink);
-        await element.click();
-    }
+//     // click the facebook button to check
+//     async clickInstagram(): Promise<void> {
+//         const element = await this.driver.findElement(this.instagramLink);
+//         await element.click();
+//     }
 
-    async getTitleText(): Promise<string> {
-        const element = await this.driver.findElement(this.title);
-        return await element.getText();
-    }
-
-//     async getSuccessMessageText(): Promise<string> {
-//         const element = await this.driver.findElement(this.successMessage);
+//     async getTitleText(): Promise<string> {
+//         const element = await this.driver.findElement(this.title);
 //         return await element.getText();
 //     }
-}
+
+// //     async getSuccessMessageText(): Promise<string> {
+// //         const element = await this.driver.findElement(this.successMessage);
+// //         return await element.getText();
+// //     }
+// }
--- test/ui/ViewJobRolesTests.ts
+++ test/ui/ViewJobRolesTests.ts
@@ -1,56 +1,164 @@
-import { Builder, Capabilities, WebDriver } from 'selenium-webdriver';
+// import { Builder, Capabilities, WebDriver, By } from 'selenium-webdriver';
+// import { expect } from 'chai';
+// import { ViewJobRolesPage } from './ViewJobRolesTestPage';
+// import * as chrome from 'selenium-webdriver/chrome';
+
+
+// // run the view job roles page
+// describe('View Job Roles Test', function () {
+//     let driver: WebDriver;
+//     let viewJobRolesPage: ViewJobRolesPage;
+
+//     before(async function () {
+//         driver = new Builder()
+//             .forBrowser('chrome')
+//             .setChromeOptions(new chrome.Options())
+//             .build();
+
+//         //viewJobRolesPage = new viewJobRolesPage(driver);
+//     });
+
+//     after(async function () {
+//         try {
+//             await driver.quit(); // Ensure this is awaited
+//         } catch (error) {
+//             console.error('Error quitting the driver:', error);
+//         }
+//     });
+
+    
+//     it('Job roles page should load', async function () {
+//         const url: string = process.env.UI_TEST_URL || 'https://jptw3amsi2.eu-west-1.awsapprunner.com/jobRoles';
+//         await viewJobRolesPage.open(url);
+
+//        // check here that things display on the view job roles page- buttons, titles etc
+
+//         const actualText = await viewJobRolesPage.getTitleText();
+//         expect(actualText).to.equal('Available Job Roles');
+//     });
+
+// // Test case: Verify that the facebook link redirects to facebook
+//     it('Facebook link should work', async function () {
+//         const url: string = process.env.UI_TEST_URL || 'https://jptw3amsi2.eu-west-1.awsapprunner.com/jobRoles';
+//         await viewJobRolesPage.open(url);  // Open the viewJobRolesPage page
+
+//         await viewJobRolesPage.clickFacebook();  // click facebook button
+//         driver.navigate().back(); // go back to kainos page
+
+// //         const actualText = await loginPage.getSuccessMessageText();  // Get the success message
+// //         expect(actualText).to.equal('Together we write our story...');  // Verify the success message
+
+//         driver.quit();
+//     });
+// });
+
+
+
+import { Builder, By, WebDriver } from 'selenium-webdriver';
 import { expect } from 'chai';
-import { ViewJobRolesPage } from './ViewJobRolesTestPage';
 import * as chrome from 'selenium-webdriver/chrome';
 
-
-// run the view job roles page
-describe('View Job Roles Test', function () {
+describe('View Job Roles Tests', function () {
     let driver: WebDriver;
-    let viewJobRolesPage: ViewJobRolesPage;
 
     before(async function () {
         driver = new Builder()
             .forBrowser('chrome')
             .setChromeOptions(new chrome.Options())
             .build();
-
-        //viewJobRolesPage = new viewJobRolesPage(driver);
     });
 
     after(async function () {
         try {
-            await driver.quit(); // Ensure this is awaited
+            await driver.quit();
         } catch (error) {
             console.error('Error quitting the driver:', error);
         }
     });
 
-    
     it('Job roles page should load', async function () {
         const url: string = process.env.UI_TEST_URL || 'https://jptw3amsi2.eu-west-1.awsapprunner.com/jobRoles';
-        await viewJobRolesPage.open(url);
+        await driver.get(url);
 
-       // check here that things display on the view job roles page- buttons, titles etc
-
-        const actualText = await viewJobRolesPage.getTitleText();
+        const title = By.xpath("//h2[normalize-space()='Available Job Roles']");
+        const element = await driver.findElement(title);
+        const actualText = await element.getText();
         expect(actualText).to.equal('Available Job Roles');
     });
 
-// Test case: Verify that the facebook link redirects to facebook
     it('Facebook link should work', async function () {
         const url: string = process.env.UI_TEST_URL || 'https://jptw3amsi2.eu-west-1.awsapprunner.com/jobRoles';
-        await viewJobRolesPage.open(url);  // Open the viewJobRolesPage page
+        await driver.get(url);
 
-        await viewJobRolesPage.clickFacebook();  // click facebook button
-        driver.navigate().back(); // go back to kainos page
+        const facebookLink = By.xpath("//a[@title='https://www.facebook.com/KainosSoftware/?locale=en_GB']");
+        const element = await driver.findElement(facebookLink);
+        await element.click();
 
-//         const actualText = await loginPage.getSuccessMessageText();  // Get the success message
-//         expect(actualText).to.equal('Together we write our story...');  // Verify the success message
+        await driver.sleep(2000); // Sleep for 2 seconds to ensure the click is processed
+        const facebookUrl = await driver.getCurrentUrl();
+        expect(facebookUrl).to.include('facebook.com/KainosSoftware');
 
-        driver.quit();
+        await driver.navigate().back();
+
+        const title = By.xpath("//h2[normalize-space()='Available Job Roles']");
+        const elementBack = await driver.findElement(title);
+        const newTitleText = await elementBack.getText();
+        expect(newTitleText).to.equal('Available Job Roles');
     });
+
+
+    it('Twitter link should work', async function () {
+        const url: string = process.env.UI_TEST_URL || 'https://jptw3amsi2.eu-west-1.awsapprunner.com/jobRoles';
+        await driver.get(url);
+
+        const twitterLink = By.xpath("//a[@title='https://x.com/i/flow/login?redirect_after_login=%2FKainosSoftware']");
+        const element = await driver.findElement(twitterLink);
+        await element.click();
+
+        await driver.sleep(2000); // Sleep for 2 seconds to ensure the click is processed
+        const twitterUrl = await driver.getCurrentUrl();
+        expect(twitterUrl).to.include('x.com');
+
+        await driver.get(url);
+
+        await driver.sleep(2000); // Sleep for 2 seconds to ensure the click is processed
+
+        const title = By.xpath("//h2[normalize-space()='Available Job Roles']");
+        const elementBack = await driver.findElement(title);
+        const newTitleText = await elementBack.getText();
+        expect(newTitleText).to.equal('Available Job Roles');
+    });
+
+
+    it('Instagram link should work', async function () {
+        const url: string = process.env.UI_TEST_URL || 'https://jptw3amsi2.eu-west-1.awsapprunner.com/jobRoles';
+        await driver.get(url);
+
+        const instagramLink = By.xpath("//a[@title='https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fkainossoftware%2F&is_from_rle']");
+        const element = await driver.findElement(instagramLink);
+        await element.click();
+
+        await driver.sleep(2000); // Sleep for 2 seconds to ensure the click is processed
+        const instagramUrl = await driver.getCurrentUrl();
+        expect(instagramUrl).to.include('instagram.com');
+
+        await driver.get(url);
+
+        await driver.sleep(2000); // Sleep for 2 seconds to ensure the click is processed
+        
+        const title = By.xpath("//h2[normalize-space()='Available Job Roles']");
+        const elementBack = await driver.findElement(title);
+        const newTitleText = await elementBack.getText();
+        expect(newTitleText).to.equal('Available Job Roles');
+    });
+
+
+
+    // this code also needs to check that the job status' all say 'open'
+    // and eventually will test that the home, jobs and login buttons work in the header
 });
+
+
 
 
 
