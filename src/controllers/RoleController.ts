747732c914e2f08cf5978853aeaaf74ded0c54ad
Anne Moore <anne.moore@kainos.com>
2024-07-29 14:37:54 +0100

    Fix URLs

--- src/controllers/RoleController.ts
+++ src/controllers/RoleController.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import { getJobRoles } from "../services/JobRoleService"
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.API_URL || 'http://localhost:3000';
+
+export const URL: string = "/";
 
 export const getHomePage = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('homepage.html');
+    res.render('homepage.html', {URL});
 }
  
 export const getAllJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        res.render('jobRolesList.html', { roles: await getJobRoles() });
+        res.render('jobRolesList.html', { URL, roles: await getJobRoles() });
     } catch (e) {
         res.locals.errormessage = e.message;
         res.render('jobRolesList.html');
