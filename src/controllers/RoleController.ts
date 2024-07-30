e94c02cc5d9ed0aea40b12249bd7e74679218184
Anne Moore <anne.moore@kainos.com>
2024-07-30 16:10:23 +0100

    Frontend tests

--- src/controllers/RoleController.ts
+++ src/controllers/RoleController.ts
@@ -2,17 +2,19 @@ import express from "express";
 import { getJobRoles } from "../services/JobRoleService"
 import axios from "axios";
 
-const instance = axios.create({
-    baseURL: process.env.AWS_URL || 'http://localhost:8080',
-  });
+axios.defaults.baseURL = process.env.AWS_URL || 'http://localhost:8080'
+
+export const URL: string = "/";
+
+export const base = URL;
 
 export const getHomePage = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('homepage.html', {instance});
+    res.render('homepage.html', {base} );
 }
  
 export const getAllJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        res.render('jobRolesList.html', { instance, roles: await getJobRoles() });
+        res.render('jobRolesList.html', { base, roles: await getJobRoles() });
     } catch (e) {
         res.locals.errormessage = e.message;
         res.render('jobRolesList.html');
--- src/services/JobRoleService.ts
+++ src/services/JobRoleService.ts
@@ -1,13 +1,15 @@
 import axios, { AxiosResponse } from "axios";
 import { JobRolesResponse } from "../models/JobRolesResponse";
 
-axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080';
+const backend = axios.create({
+	baseURL: process.env.API_URL || 'http://localhost:8080',
+});
 
 export const URL: string = "/api/job-roles/";
 
 export const getJobRoles = async (): Promise<JobRolesResponse[]> => {
     try {
-        const response: AxiosResponse = await axios.get(URL);
+        const response: AxiosResponse = await backend.get(URL);
 
         return response.data;
     } catch (e) {
