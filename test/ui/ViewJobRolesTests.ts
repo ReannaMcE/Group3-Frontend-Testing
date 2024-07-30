3a3c18c8af8f6d603833073e4e8b315f50d282bf
Reanna <reanna.mceldowney@kainos.com>
2024-07-30 15:23:31 +0100

    added test yaml file

--- test/ui/ViewJobRolesTests.ts
+++ test/ui/ViewJobRolesTests.ts
@@ -1,77 +1,100 @@
-import { Builder, WebDriver } from 'selenium-webdriver';
-import { expect } from 'chai';
-import * as chrome from 'selenium-webdriver/chrome';
 import { ViewJobRolesTestsPage } from './ViewJobRolesTestsPage';
 
 describe('View Job Roles Tests', function () {
     this.timeout(50000);
-    let driver: WebDriver;
 
-    // Before all tests
     before(async function () {
-        driver = new Builder()
-            .forBrowser('chrome')
-            .setChromeOptions(new chrome.Options())
-            .build();
+        await ViewJobRolesTestsPage.startDriver();
     });
 
-    // After all tests
     after(async function () {
-        try {
-            await driver.quit();
-        } catch (error) {
-            console.error('Error quitting the driver:', error);
-        }
+        await ViewJobRolesTestsPage.quitDriver();
     });
 
-    // Test for loading the job roles page
     it('Job roles page should load', async function () {
-        await ViewJobRolesTestsPage.loadPage(driver);
-        await ViewJobRolesTestsPage.assertJobRolesTitle(driver);
+        await ViewJobRolesTestsPage.loadPage();
+        await ViewJobRolesTestsPage.assertJobRolesTitle();
     });
 
-    // Test for Facebook link
     it('Facebook link should work', async function () {
         try {
-            await ViewJobRolesTestsPage.loadPage(driver);
-            await ViewJobRolesTestsPage.clickFacebook(driver);
-            await ViewJobRolesTestsPage.assertFacebook(driver);
-            await driver.navigate().back();
-            await ViewJobRolesTestsPage.assertJobRolesTitle(driver);
+            await ViewJobRolesTestsPage.loadPage();
+            await ViewJobRolesTestsPage.clickFacebook();
+            await ViewJobRolesTestsPage.assertFacebook();
+            await ViewJobRolesTestsPage.getDriver().navigate().back();
+            await ViewJobRolesTestsPage.assertJobRolesTitle();
         } catch (error) {
             console.error('Test failed:', error);
             throw error;
         }
     });
 
-    // Test for Twitter link
     it('Twitter link should work', async function () {
         try {
-            await ViewJobRolesTestsPage.loadPage(driver);
-            await ViewJobRolesTestsPage.clickTwitter(driver);
-            await ViewJobRolesTestsPage.assertTwitter(driver);
-            await ViewJobRolesTestsPage.loadPage(driver);
-            await ViewJobRolesTestsPage.assertJobRolesTitle(driver);
+            await ViewJobRolesTestsPage.loadPage();
+            await ViewJobRolesTestsPage.clickTwitter();
+            await ViewJobRolesTestsPage.assertTwitter();
+            await ViewJobRolesTestsPage.loadPage();
+            await ViewJobRolesTestsPage.assertJobRolesTitle();
         } catch (error) {
             console.error('Test failed:', error);
             throw error;
         }
     });
 
-    // Test for Instagram link
     it('Instagram link should work', async function () {
         try {
-            await ViewJobRolesTestsPage.loadPage(driver);
-            await ViewJobRolesTestsPage.clickInstagram(driver);
-            await ViewJobRolesTestsPage.assertInstagram(driver);
-            await ViewJobRolesTestsPage.loadPage(driver);
-            await ViewJobRolesTestsPage.assertJobRolesTitle(driver);
+            await ViewJobRolesTestsPage.loadPage();
+            await ViewJobRolesTestsPage.clickInstagram();
+            await ViewJobRolesTestsPage.assertInstagram();
+            await ViewJobRolesTestsPage.loadPage();
+            await ViewJobRolesTestsPage.assertJobRolesTitle();
         } catch (error) {
             console.error('Test failed:', error);
             throw error;
         }
     });
 
-    // This code also needs to check that the job statuses all say 'open'
-    // and eventually will test that the home, jobs, and login buttons work in the header
+    // it('Job status should always be open', async function () {
+    //     await ViewJobRolesTestsPage.loadPage();
+    //     await ViewJobRolesTestsPage.assertJobStatusOpen();
+    // });
+
+    // it('Home button should work', async function () {
+    //     try {
+    //         await ViewJobRolesTestsPage.loadPage();
+    //         await ViewJobRolesTestsPage.clickHome();
+    //         await ViewJobRolesTestsPage.assertHomePageImg();
+    //         await ViewJobRolesTestsPage.loadPage();
+    //         await ViewJobRolesTestsPage.assertJobRolesTitle();
+    //     } catch (error) {
+    //         console.error('Test failed:', error);
+    //         throw error;
+    //     }
+    // });
+
+    // it('Jobs button should work', async function () {
+    //     try {
+    //         await ViewJobRolesTestsPage.loadPage();
+    //         await ViewJobRolesTestsPage.clickJobs();
+    //         await ViewJobRolesTestsPage.assertJobRolesTitle();
+    //     } catch (error) {
+    //         console.error('Test failed:', error);
+    //         throw error;
+    //     }
+    // });
+
+    // it('Login button should work', async function () {
+    //     try {
+    //         await ViewJobRolesTestsPage.loadPage();
+    //         await ViewJobRolesTestsPage.clickLogin();
+    //         await ViewJobRolesTestsPage.assertLoginPage();
+    //         await ViewJobRolesTestsPage.loadPage();
+    //         await ViewJobRolesTestsPage.assertJobRolesTitle();
+    //     } catch (error) {
+    //         console.error('Test failed:', error);
+    //         throw error;
+    //     }
+    // });
+
 });
--- test/ui/ViewJobRolesTestsPage.ts
+++ test/ui/ViewJobRolesTestsPage.ts
@@ -1,47 +1,97 @@
-import { WebDriver, WebElement, By, until } from 'selenium-webdriver';
+import { WebDriver, WebElement, By, until, Builder } from 'selenium-webdriver';
 import { expect } from 'chai';
+import * as chrome from 'selenium-webdriver/chrome';
 
 export class ViewJobRolesTestsPage {
+    private static driver: WebDriver;
+
+
+
+    // WebDriver Functions --------------------------------------------------------------------------------
+    public static async startDriver(): Promise<void> {
+        this.driver = await new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(new chrome.Options())
+            .build();
+    }
+
+    public static async quitDriver(): Promise<void> {
+        if (this.driver) {
+            await this.driver.quit();
+        }
+    }
+
+    public static getDriver(): WebDriver {
+        return this.driver;
+    }
+
 
 
     // Load Page --------------------------------------------------------------------------------
     private static BASE_URL: string = process.env.UI_TEST_URL || 'https://jptw3amsi2.eu-west-1.awsapprunner.com/jobRoles';
 
-    public static async loadPage(driver: WebDriver): Promise<void> {
-        await driver.get(this.BASE_URL);
+    public static async loadPage(): Promise<void> {
+        await this.driver.get(this.BASE_URL);
     }
+
+
 
     // Web Elements --------------------------------------------------------------------------------
-    public static async FacebookLink(driver: WebDriver): Promise<WebElement> {
-        return driver.findElement(By.xpath("//a[@title='https://www.facebook.com/KainosSoftware/?locale=en_GB']"));
+    // delete these three when id has been merged !
+    public static async FacebookLink(): Promise<WebElement> {
+        return this.driver.findElement(By.xpath("//a[@title='https://www.facebook.com/KainosSoftware/?locale=en_GB']"));
     }
 
-    public static async TwitterLink(driver: WebDriver): Promise<WebElement> {
-        return driver.findElement(By.xpath("//a[@title='https://x.com/i/flow/login?redirect_after_login=%2FKainosSoftware']"));
+    public static async TwitterLink(): Promise<WebElement> {
+        return this.driver.findElement(By.xpath("//a[@title='https://x.com/i/flow/login?redirect_after_login=%2FKainosSoftware']"));
     }
 
-    public static async InstagramLink(driver: WebDriver): Promise<WebElement> {
-        return driver.findElement(By.xpath("//a[@title='https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fkainossoftware%2F&is_from_rle']"));
+    public static async InstagramLink(): Promise<WebElement> {
+        return this.driver.findElement(By.xpath("//a[@title='https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fkainossoftware%2F&is_from_rle']"));
     }
 
-    // public static async HomeButton(driver: WebDriver): Promise<WebElement> {
-    //     return driver.findElement(By.id("id of home button goes here ******"));
+    // public static async FacebookLink(): Promise<WebElement> {
+    //     return this.driver.findElement(By.id("facebook"));
     // }
 
-    // public static async LoginButton(driver: WebDriver): Promise<WebElement> {
-    //     return driver.findElement(By.id("**** id of the login button goes here *****"));
+    // public static async TwitterLink(): Promise<WebElement> {
+    //     return this.driver.findElement(By.id("twitter"));
     // }
 
-    // public static async JobRolesButton(driver: WebDriver): Promise<WebElement> {
-    //     return driver.findElement(By.id("*** id of the job roles button goes here ****"));
+    // public static async InstagramLink(): Promise<WebElement> {
+    //     return this.driver.findElement(By.id("instagram"));
     // }
 
+    // public static async HomeButton(): Promise<WebElement> {
+    //     return this.driver.findElement(By.id("navbarHome"));
+    // }
+
+    // public static async LoginButton(): Promise<WebElement> {
+    //     return this.driver.findElement(By.id("navbarLogin"));
+    // }
+
+    // * this is for the job roles page
+    // public static async JobRolesButton(): Promise<WebElement> {
+    //     return this.driver.findElement(By.id("navbarJobs"));
+    // }
+
+    // public static async JobStatusElement(): Promise<WebElement> {
+    //     return this.driver.findElement(By.id("jobStatus"));
+    // }
+
+    // public static async HomePageImg(): Promise<WebElement> {
+    //     return this.driver.findElement(By.id("homepageBackgroundImage"));
+    // }
+
+    // public static async LoginUserTextBox(): Promise<WebElement> {
+    //     return this.driver.findElement(By.id("username"));
+    // }
 
     // Click Links --------------------------------------------------------------------------------
-    public static async clickFacebook(driver: WebDriver): Promise<void> {
+    public static async clickFacebook(): Promise<void> {
         try {
-            const facebookLink: WebElement = await this.FacebookLink(driver);
-            await driver.wait(until.elementIsVisible(facebookLink), 10000);
+            const facebookLink: WebElement = await this.FacebookLink();
+            await this.driver.wait(until.elementIsVisible(facebookLink), 10000);
             await facebookLink.click();
         } catch (error) {
             console.error('Error clicking the Facebook link:', error);
@@ -49,10 +99,10 @@ export class ViewJobRolesTestsPage {
         }
     }
 
-    public static async clickTwitter(driver: WebDriver): Promise<void> {
+    public static async clickTwitter(): Promise<void> {
         try {
-            const twitterLink: WebElement = await this.TwitterLink(driver);
-            await driver.wait(until.elementIsVisible(twitterLink), 10000);
+            const twitterLink: WebElement = await this.TwitterLink();
+            await this.driver.wait(until.elementIsVisible(twitterLink), 10000);
             await twitterLink.click();
         } catch (error) {
             console.error('Error clicking the Twitter link:', error);
@@ -60,10 +110,10 @@ export class ViewJobRolesTestsPage {
         }
     }
 
-    public static async clickInstagram(driver: WebDriver): Promise<void> {
+    public static async clickInstagram(): Promise<void> {
         try {
-            const instagramLink: WebElement = await this.InstagramLink(driver);
-            await driver.wait(until.elementIsVisible(instagramLink), 10000);
+            const instagramLink: WebElement = await this.InstagramLink();
+            await this.driver.wait(until.elementIsVisible(instagramLink), 10000);
             await instagramLink.click();
         } catch (error) {
             console.error('Error clicking the Instagram link:', error);
@@ -71,34 +121,97 @@ export class ViewJobRolesTestsPage {
         }
     }
 
+
+    // public static async clickHome(): Promise<void> {
+    //     try {
+    //         const homeButton: WebElement = await this.HomeButton();
+    //         await this.driver.wait(until.elementIsVisible(homeButton), 10000);
+    //         await homeButton.click();
+    //     } catch (error) {
+    //         console.error('Error clicking the home button:', error);
+    //         throw error;
+    //     }
+    // }
+
+    // public static async clickJobs(): Promise<void> {
+    //     try {
+    //         const jobRolesButton: WebElement = await this.JobRolesButton();
+    //         await this.driver.wait(until.elementIsVisible(jobRolesButton), 10000);
+    //         await jobRolesButton.click();
+    //     } catch (error) {
+    //         console.error('Error clicking the jobs button:', error);
+    //         throw error;
+    //     }
+    // }
+
+    // public static async clickLogin(): Promise<void> {
+    //     try {
+    //         const loginButton: WebElement = await this.LoginButton();
+    //         await this.driver.wait(until.elementIsVisible(loginButton), 10000);
+    //         await loginButton.click();
+    //     } catch (error) {
+    //         console.error('Error clicking the login button:', error);
+    //         throw error;
+    //     }
+    // }
+
     
+
     // Assertions --------------------------------------------------------------------------------
-    public static async assertFacebook(driver: WebDriver): Promise<void> {
-        await driver.wait(async () => {
-            const currentUrl = await driver.getCurrentUrl();
+    public static async assertFacebook(): Promise<void> {
+        await this.driver.wait(async () => {
+            const currentUrl = await this.driver.getCurrentUrl();
             return currentUrl.includes('facebook.com/KainosSoftware');
         }, 15000);
     }
 
-    public static async assertTwitter(driver: WebDriver): Promise<void> {
-        await driver.wait(async () => {
-            const currentUrl = await driver.getCurrentUrl();
+    public static async assertTwitter(): Promise<void> {
+        await this.driver.wait(async () => {
+            const currentUrl = await this.driver.getCurrentUrl();
             return currentUrl.includes('x.com');
         }, 15000);
     }
 
-    public static async assertInstagram(driver: WebDriver): Promise<void> {
-        await driver.wait(async () => {
-            const currentUrl = await driver.getCurrentUrl();
+    public static async assertInstagram(): Promise<void> {
+        await this.driver.wait(async () => {
+            const currentUrl = await this.driver.getCurrentUrl();
             return currentUrl.includes('instagram.com');
         }, 15000);
     }
 
-    public static async assertJobRolesTitle(driver: WebDriver): Promise<void> {
+    // delete this when the id is available and replace with the test below
+    public static async assertJobRolesTitle(): Promise<void> {
         const title = By.xpath("//h2[normalize-space()='Available Job Roles']");
-        await driver.wait(until.elementLocated(title), 10000);
-        const elementBack = await driver.findElement(title);
+        await this.driver.wait(until.elementLocated(title), 10000);
+        const elementBack = await this.driver.findElement(title);
         const newTitleText = await elementBack.getText();
         expect(newTitleText).to.equal('Available Job Roles');
     }
+
+    // public static async assertJobRolesTitle(): Promise<void> {
+    //     const title = By.id("availableJobRoles");
+    //     await this.driver.wait(until.elementLocated(title), 10000);
+    //     const elementBack = await this.driver.findElement(title);
+    //     const newTitleText = await elementBack.getText();
+    //     expect(newTitleText).to.equal('Available Job Roles');
+    // }
+
+    // public static async assertJobStatusOpen(): Promise<void> {
+    //     const jobStatusElement = await this.JobStatusElement();
+    //     const jobStatusText = await jobStatusElement.getText();
+    //     expect(jobStatusText).to.equal('Status: open');
+    // }
+
+    // public static async assertHomePageImg(): Promise<void> {
+    //     await this.driver.wait(async () => {
+    //         this.HomePageImg() != null;
+    //     }, 15000);
+    // }
+
+    // public static async assertLoginPage(): Promise<void> {
+    //     await this.driver.wait(async () => {
+    //         this.LoginUserTextBox() != null;
+    //     }, 15000);
+    // }
+
 }
