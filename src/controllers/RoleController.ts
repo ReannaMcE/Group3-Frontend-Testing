a034664e3384a15d1aa61afee01bd0b29e707274
Anne Moore <anne.moore@kainos.com>
2024-07-31 09:20:45 +0100

    Fix URLs using base url

--- src/controllers/RoleController.ts
+++ src/controllers/RoleController.ts
@@ -2,19 +2,17 @@ import express from "express";
 import { getJobRoles } from "../services/JobRoleService"
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.AWS_URL || 'http://localhost:8080'
+axios.defaults.baseURL = process.env.AWS_URL || 'http://localhost:3000';
 
-export const URL: string = "/";
-
-export const base = URL;
+const baseURL = axios.defaults.baseURL
 
 export const getHomePage = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('homepage.html', {base} );
+    res.render('homepage.html', {baseURL} );
 }
  
 export const getAllJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        res.render('jobRolesList.html', { base, roles: await getJobRoles() });
+        res.render('jobRolesList.html', { baseURL, roles: await getJobRoles() });
     } catch (e) {
         res.locals.errormessage = e.message;
         res.render('jobRolesList.html');
