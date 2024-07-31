13bbd5fcda29212d6145b0bb036594da67897744
ashleighB31 <ashleigh.brennan@kainos.com>
2024-07-31 10:04:17 -0700

    code and files tidied, linting checked and tests running

--- src/Routes/AuthRoutes.ts
+++ src/Routes/AuthRoutes.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-import { logout } from '../controllers/AuthController';
+//import { logout } from '../controllers/AuthController';
 
 const router = express.Router();
 
-router.post('/api/auth/logout', logout);
+//router.post('/api/auth/logout', logout);
 
 export default router;
--- test/unit/controller/AuthControllerTest.ts
+++ test/unit/controller/AuthControllerTest.ts
@@ -2,51 +2,52 @@ import sinon from "sinon";
 import * as AuthService from "../../../src/services/AuthService";
 import * as AuthController from "../../../src/controllers/AuthController";
 import { expect } from "chai";
-
-
+ 
+ 
 const mockToken = "mockedtoken";
-
-
+ 
+ 
 describe('AuthController', function () {
     afterEach(() => {
         sinon.restore();
     });
-
+ 
     describe('getLoginForm', function () {
       it('should login successfully with genuine credentials', async () => {
+        sinon.stub(AuthService, 'getToken').resolves(mockToken);
         const req = { body: {username: "user", password: "user" }, session: {token: ""}};
         const res = {redirect: sinon.spy(), render: sinon.spy()};
-
+ 
         await AuthController.postLoginForm(req as any, res as any); // eslint-disable-line  @typescript-eslint/no-explicit-any
-
+ 
         expect(mockToken == req.session.token).to.be.true;
         expect(res.redirect.calledOnce).to.be.true;
       });
-
+ 
       it('should render the login form', async () => {
         const res = {redirect: sinon.spy(), render: sinon.spy()};
         const req = { body: {username: "user", password: "user" }, session: {token: ""}};
-
+ 
         await AuthController.getLoginForm(req as any, res as any); // eslint-disable-line  @typescript-eslint/no-explicit-any
-
+ 
         expect(res.render.calledOnce).to.be.true;
         expect(res.render.calledWith('loginForm.html'));
         });
-
-      it('should throw an error when login fails with incorrect credentiatls', async () => { 
+ 
+      it('should throw an error when login fails with incorrect credentiatls', async () => {
         const res = {redirect: sinon.spy(), render: sinon.spy(), locals: { errormessage: ''}};
         const req = { body: {username: "wronguser", password: "wrongpassword" }, session: {token: ""}};
         
         const errorMessage: string = 'Error message';
         sinon.stub(AuthService, 'getToken').rejects(new Error(errorMessage));
-
+ 
         
         await AuthController.postLoginForm(req as any, res as any); // eslint-disable-line  @typescript-eslint/no-explicit-any
-
+ 
         expect(res.render.calledOnce).to.be.true;
         expect(res.render.calledWith('loginForm.html')).to.be.true;
         expect(res.locals.errormessage).to.equal(errorMessage);
-
+ 
       });
     });
 });
