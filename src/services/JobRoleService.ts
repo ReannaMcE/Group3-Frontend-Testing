ce845a76ac020c1319c79159dde4b101aced81ee
Anne Moore <anne.moore@kainos.com>
2024-08-01 15:57:34 +0100

    Fixing merge conflicts and testing errors

--- src/services/JobRoleService.ts
+++ src/services/JobRoleService.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import { JobRolesResponse } from "../models/JobRolesResponse";
+import { JobRole } from "../models/JobRole";
 
 axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080';
 
@@ -12,5 +13,22 @@ export const getJobRoles = async (): Promise<JobRolesResponse[]> => {
         return response.data;
     } catch (e) {
         throw new Error('Failed to get Job Roles');
+    }
+}
+
+export const getJobRoleById = async (id: string): Promise<JobRole> => {
+    try {
+        const response: AxiosResponse = await axios.get(URL + id);
+
+        if(response.data.status != "open") {
+            return null;
+        }
+
+        return response.data;
+    } catch (e) { 
+        if(e.response.status == 404) {
+            throw new Error("Job Role does not exist");
+        }
+        throw new Error('Failed to get Job Role');
     }
 }
